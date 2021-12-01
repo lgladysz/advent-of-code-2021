3417a2da5981f086769f9c806f4dc4496deb6890
Łukasz Gładysz <lukasz.gladysz@efinity.com>
2021-12-01 10:44:08 +0100

    [Day01] Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part1(input: List<Int>): Int {
+    var result = 0
+    var previousNumber = input.first()
+
+    for (currentNumber in input) {
+      if (currentNumber > previousNumber) {
+        result += 1
+      }
+      previousNumber = currentNumber
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    return result
+  }
+
+  fun part2(input: List<List<Int>>): Int {
+    var result = 0
+    var previousNumbers = input.first()
+
+    for (currentNumbers in input) {
+      if (currentNumbers.sum() > previousNumbers.sum()) {
+        result += 1
+      }
+      previousNumbers = currentNumbers
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    return result
+  }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day01_test").map { it.toInt() }
+  check(part1(testInput) == 7)
+  check(part2(testInput.windowed(3)) == 5)
+
+  val input = readInput("Day01").map { it.toInt() }
+  println(part1(input))
+  println(part2(input.windowed(3)))
 }
